474d05443c5a4aeb4b1f48c905425aa6ac209f30
shwewinaung <nickytwist70@gmail.com>
2025-09-06 21:38:32 +0530

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { LanguageProvider } from "./context/languageContext.js";
+import { LanguageProvider } from "./context/languageContext.tsx";
 import "./index.css";
 import App from "./App.jsx";
 
